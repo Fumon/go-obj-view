b3d1c4ae4c9eac2f7184cb438fb2c015bc6beedc
Geordie Bilkey <himself@thefumon.com>
2011-10-30 19:05:03 -0400

    Spinning Triangle with small matrix library

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"gl"
 	"github.com/jteeuwen/glfw"
+	"math"
 	"unsafe"
 )
 
@@ -21,6 +22,7 @@ var (
 	program gl.Program
 	attrib_loc gl.AttribLocation
 	colattrib gl.AttribLocation
+	transformattrib gl.UniformLocation
 
 	vert_poscolor = []poscol{ //Three Position followed by three color
 		{Position: [3]float32{0.0, 0.8, 0.0}, Color: [3]float32{1.0, 0.0, 0.0}},
@@ -29,6 +31,9 @@ var (
 	}
 	sizeofposcol int
 	offsettocolor int
+
+	//Tick stuff
+	//lasttime float64
 )
 
 type poscol struct {
@@ -80,6 +85,7 @@ func main() {
 
 	running = true
 	for running && glfw.WindowParam(glfw.Opened) == 1 {
+		calc_tick()
 		draw()
 		glfw.SwapBuffers()
 		if glfw.Key('Q') == glfw.KeyPress {
@@ -122,6 +128,14 @@ func draw() {
 
 	//Draw from array.
 	gl.DrawArrays(gl.TRIANGLES, 0, 3)
+}
+
+func calc_tick() {
+	//Set the uniform
+	angle := float32(glfw.Time() * 4.5) //45 degrees a second
+	z_axis := []float32{0.0,0.0,1.0}
+	transform := AxisAngleRotation(z_axis, angle)
+	transformattrib.UniformMatrix4fv(1, false, transform[:])
 }
 
 func init_resources() (err os.Error) {
@@ -183,6 +197,11 @@ func init_program() (err os.Error) {
 		program.Delete()
 		err = os.NewError("Attribute not located")
 	}
+	if transformattrib = program.GetUniformLocation("m_transform"); transformattrib == -1 {
+		fmt.Fprintf(os.Stderr, "Failed to find transform attribute location %v\n", program.GetInfoLog())
+		program.Delete()
+		err = os.NewError("Attribute not located")
+	}
 
 	return
 }
@@ -208,5 +227,86 @@ func loadshader(filename string, shType gl.GLenum) (sh gl.Shader, err os.Error) 
 		err = os.NewError("Compile error")
 		return
 	}
+	return
+}
+
+
+//Matrix stuff
+
+//A column major ordered matrix
+type mat4 [16]float32
+
+func MakeMat4(a float32) (m *mat4) {
+	m = new(mat4)
+	for i := range (*m) {
+		(*m)[i] = a
+	}
+	return
+}
+
+func IdMat4() (m *mat4) {
+	m = new(mat4)
+	for i := range m {
+		switch i {
+		case 0, 5, 10, 15: m[i] = 1.0
+		default: m[i] = 0.0
+		}
+	}
+	return
+}
+
+//Remember, column major and zero indexed
+func (m *mat4) At(row, col int) float32 {
+	return (*m)[4*col + row]
+}
+
+//a * b in written order
+func (a mat4) Product(b mat4) (mv *mat4) {
+	mv = new(mat4)
+	for col := 0; col < 4; col++ {
+		for row := 0; row < 0; row++ {
+			var sum float32
+			for i := 0; i < 4; i++ {
+				sum += a.At(row, i) * b.At(i, col)
+			}
+			mv[col*4 + row] = sum
+		}
+	}
+
+	return
+}
+
+func AxisAngleRotation(axis []float32, angle float32) (mv *mat4) {
+	mv = MakeMat4(1.0)
+	c := math.Cos(float64(angle))
+	s := math.Sin(float64(angle))
+	t := 1 - c
+	x := float64(axis[0])
+	y := float64(axis[1])
+	z := float64(axis[2])
+	//Exploded
+	mv[0] = float32(t * x * x + c)
+	mv[1] = float32(t * x * y + z * s)
+	mv[2] = float32(t * x * z - y * s)
+
+	mv[3] = float32(0.0)
+
+	mv[4] = float32(t * x * y - z * s)
+	mv[5] = float32(t * y * y + c)
+	mv[6] = float32(t * y * z + x * s)
+
+	mv[7] = float32(0.0)
+
+	mv[8] = float32(t * x * z + y * s)
+	mv[9] = float32(t * y * z - x * s)
+	mv[10] = float32(t * z * z + c)
+
+	mv[11] = float32(0.0)
+
+	mv[12] = float32(0.0)
+	mv[13] = float32(0.0)
+	mv[14] = float32(0.0)
+	mv[15] = float32(1.0)
+
 	return
 }
