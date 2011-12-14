2f25805c06db3188c276e5afdcca93c7926fac95
Geordie Bilkey <himself@thefumon.com>
2011-12-14 08:31:59 -0500

    Directional per-pixel lighting doesn't seem to do much

--- cube.f.glsl
+++ cube.f.glsl
@@ -1,5 +1,25 @@
 #version 120
-varying vec4 f_color;
+varying vec3 vary_norm;
+
+struct lightsource {
+	vec4 position;
+	vec4 diffuse;
+};
+lightsource light0 = lightsource(
+	vec4(1.0, 1.0, 1.0, 0.0),
+	vec4(1.0, 1.0, 1.0, 1.0)
+);
+
+struct material {
+	vec4 diffuse;
+};
+material monkey = material(vec4(1.0, 0.8, 0.8, 1.0));
+
 void main(void) {
-	gl_FragColor = f_color;
+
+	vec3 normal_dir = normalize(vary_norm);
+	vec3 light_dir = normalize(vec3(light0.position));
+	
+	float d_r = max(0.0, dot(normal_dir, light_dir));
+	gl_FragColor = vec4(vec3(light0.diffuse) * vec3(monkey.diffuse) * d_r, 1.0);
 }
